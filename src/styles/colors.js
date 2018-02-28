851656361da4d0095bfc1bb9fada7d7063179dce
Travis Raghibzadeh <travisraghib@apple.com>
2018-02-28 11:25:04 -0800

    starting commit

---
+++ src/styles/colors.js
@@ -0,0 +1,28 @@
+module.exports = {
+  first: '#EEEEEE',
+  firstLight: '#6C7A89',
+  firstSuperLight: 'black',
+  firstDark: 'blue',
+
+  accent: 'pink',
+  bright: '#BFBFBF',
+  light: 'olive',
+  middle: '#666666',
+  dark: '#333333',
+  superDark: '#111111',
+
+  white: 'white',
+  black: 'black',
+  grey100: '#5D5F6D',
+  grey90: '#747787',
+  grey80: '#8A8FA1',
+  grey70: '#A0A6BA',
+  grey60: '#B8BED4',
+  grey50: '#CED5ED',
+  grey40: '#DEE6FF',
+
+  red: 'red',
+
+
+
+};
---
+++ src/styles/theme.js
@@ -0,0 +1,97 @@
+const colors = require("./colors");
+
+const theme = {
+  navigator: {
+    colors: {
+      title: colors.grey100,
+      header: colors.grey100,
+      subTitle: colors.grey80,
+      scrollTrack: colors.red,
+      scrollThumb: colors.red,
+      linkHover: colors.grey60,
+      asideItemActiveBorder: colors.accent
+    },
+    sizes: {
+      asideWidth: "19em",
+      maxWidth: "56em"
+    },
+    backgrounds: {
+      wrapper: colors.white,
+      asideItemActive: colors.firstDark
+    }
+  },
+  post: {
+    colors: {
+      author: colors.grey90,
+      authorBorder: colors.grey40,
+      bold: colors.grey100,
+      blockquoteFrame: colors.grey40,
+      copyright: colors.grey60,
+      link: colors.grey70,
+      linkHover: colors.grey60,
+      meta: colors.grey100,
+      metaBorder: colors.grey100,
+      text: colors.grey100,
+      title: colors.grey100,
+      subTitle: colors.grey80
+    },
+    backgrounds: {
+      wrapper: colors.white,
+      meta: colors.white
+    },
+    sizes: {
+      maxWidth: "50em"
+    }
+  },
+  bottomBar: {
+    colors: {
+      link: colors.bright,
+      icon: colors.firstSuperLight
+    },
+    backgrounds: {
+      wrapper: colors.first,
+      icon: colors.firstDark
+    },
+    sizes: {
+      height: 44 //pixels
+    }
+  },
+  topBar: {
+    colors: {
+      logo: colors.bright,
+      logoPost: colors.first
+    },
+    backgrounds: {
+      wrapper: colors.white,
+      wrapperPost: colors.white,
+      icon: colors.light
+    },
+    sizes: {
+      height: 44 //pixels
+    }
+  },
+  info: {
+    colors: {
+      text: colors.firstDark,
+      link: colors.firstDark,
+      linkHover: colors.first,
+      btn: colors.bright
+    },
+    backgrounds: {
+      wrapper: colors.firstLight,
+      btn: colors.accent
+    },
+    sizes: {
+      maxWidth: "40em"
+    }
+  },
+  mediaQueryTresholds: {
+    XL: "65em",
+    L: "49em",
+    M: "37em",
+    S: "28em",
+    XS: "21em"
+  }
+};
+
+export default theme;
