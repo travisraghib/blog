258612eacaf1a339959fbf3f01dd1fc80dea3d32
Travis Raghib <travisraghib@gmail.com>
2018-03-27 12:41:16 -0700

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-323bb8d725ca39fa520a.js","2b5e68ee801efe214462078d255731a4"],["/app-53c6127423bde5ed32b3.js","cd223ad2f7aae6ff1885b56cb840915a"],["/app-6a4a62cd9605230de67d.js","253e01555e0b609b3b42d963d15dc8d1"],["/app-78cac09023d8ff9e3841.js","a2641b9059f5e10c770cb9286491fb39"],["/app-8953eada07685726c1a6.js","f56365ba3e12dac9e48e85452604f94e"],["/app-a753da3616dcbf07f0d6.js","5be17e9a780f4f39601441e56cd59a4d"],["/app-b41afa56ba9447e3827c.js","03ac8e833d1419c585a933914d17979d"],["/commons-4558505f1b2c85ff7eb3.js","9a48daccd8ab6ae3263600a73caf5980"],["/index.html","7137f32f65f4e415ef03d12b271c9a74"],["/manifest.json","ef2b6af754d74b47c07335d5ce110f7d"],["/offline-plugin-app-shell-fallback/index.html","aaca91d8db1285ca1c7461ca0f5241ad"]];
+var precacheConfig = [["/app-94c65ef3e02013adf3df.js","ab451dd077b452e06061ca8ab3d4db39"],["/commons-bd3a6ed8202ff2bb204e.js","085598f0a695f52f83b75c6f0fff8b6e"],["/index.html","9304f2d562c3dd4e38dfc20808f863d2"],["/manifest.json","a36cee9dbf5f02ad61a5d124c1a7d8d5"],["/offline-plugin-app-shell-fallback/index.html","37f5e67525ba66ba74698160a81073a6"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
